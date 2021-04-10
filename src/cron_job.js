9334ecb7ea0d108c2a8868d64e46a230dea84a12
Thiago-Ferreira-Silva <62727996+Thiago-Ferreira-Silva@users.noreply.github.com>
2021-04-10 11:37:05 -0300

    Implements a cron job to update the db

---
+++ src/cron_job.js
@@ -0,0 +1,12 @@
+const cron = require('node-cron')
+const db = require('./db')
+const scraper = require('./scraper')
+
+function startJob() {
+    cron.schedule('0 0 */2 * *', async () => {
+        const result = await scraper.fetchHTML()
+        db.addNewData(result)
+    })
+}
+
+module.exports = { startJob }
--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 const express = require('express')
-const fetchHTML = require('./scraper')
+const scraper = require('./scraper')
+const cron = require('./cron_job')
 
 const PORT = process.env.PORT || 8081
 
 const app = express()
 
+cron.startJob()
+
 app.get('/', async (req, res) => {
-    const response = await fetchHTML()
+    const response = await scraper.fetchHTML()
     res.status(response.status).send(response)
 })
 
-//criar um banco de dados e armazenar o histórico do ranking, atualizando as informações uma vez por dia ou semana com o node cron
+// implementar app.get('/data') com histórico de dados e talvez alguma coisa de estatística
+
+// criar um banco de dados e armazenar o histórico do ranking, atualizando as informações uma vez por dia ou semana com o node cron
+
 // escrever testes
+
+// Adicionar a função de fazer backup periódico da anime list de um usuário no projeto web-scraping
 
 app.listen(PORT, () => console.log('Running on PORT: ', PORT))
